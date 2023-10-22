cda46e473029419e9326533abc8cde43860566c2
WillowWisp <ckurschat@aol.com>
2023-10-22 00:32:46 -0400

    Code Cleanups
    Fixed crashes from scavenger AI, player references, and other things

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -24,7 +24,7 @@ namespace GuideSlugBase
     [BepInPlugin(MOD_ID, "Guide", "0.2.0")]
     class Plugin : BaseUnityPlugin
     {
-        private const string MOD_ID = "aveskori.applecat";
+        private const string MOD_ID = "aveskori.guide";
 
 
         // Add hooks
@@ -46,37 +46,71 @@ namespace GuideSlugBase
             On.ScavengerAI.CollectScore_ItemRepresentation_bool += ScavengerAI_CollectScore_ItemRepresentation_bool1;
             On.ScavengerAI.DecideBehavior += ScavengerAI_DecideBehavior;
             //ScavengerBehaviorModification.Hooks();   < very not done. currently breaks scavs entirely lmao
-            
+            On.ScavengerAI.SocialEvent += ScavengerAI_SocialEvent; //GUIDE TAKING SCAV ITEMS DOESN'T DECREASE REP
+
+
         }
 
-        
+        private void ScavengerAI_SocialEvent(On.ScavengerAI.orig_SocialEvent orig, ScavengerAI self, SocialEventRecognizer.EventID ID, Creature subjectCrit, Creature objectCrit, PhysicalObject involvedItem)
+        {
+            //GUIDE CAN COMMIT CRIMES...
+            if (subjectCrit is Player && (subjectCrit as Player).slugcatStats.name.value == "Guide")
+		    {
+                if (ID == SocialEventRecognizer.EventID.Theft 
+                    || ID == SocialEventRecognizer.EventID.NonLethalAttackAttempt
+                    || ID == SocialEventRecognizer.EventID.NonLethalAttack
+                    || ID == SocialEventRecognizer.EventID.LethalAttackAttempt)
+                {
+                    return; //JUST PRETEND IT DIDN'T HAPPEN...
+                }
+            }
+            orig(self, ID, subjectCrit, objectCrit, involvedItem);
+        }
+
         private int ScavengerAI_CollectScore_ItemRepresentation_bool1(On.ScavengerAI.orig_CollectScore_ItemRepresentation_bool orig, ScavengerAI self, ItemTracker.ItemRepresentation obj, bool weaponFiltered)
         {
-            if ((self.scavenger.room.game.Players[0].realizedCreature as Player).slugcatStats.name.value == "Guide")
-            {
-                
-
-                /*if (obj.representedItem is )
-                {
+            //JUST COMMENT THIS OUT UNTIL IT IS IN USE
+            //if ((self.scavenger.room.game.Players[0].realizedCreature as Player).slugcatStats.name.value == "Guide")
+            //{
+            //    /*if (obj.representedItem is )
+            //    {
                     
-                }*/
-
-            }
+            //    }*/
+            //}
             return orig(self, obj, weaponFiltered);
         }
 
+        public static Player FindNearbyGuide(Room myRoom)
+        {
+            if (myRoom == null)
+                return null; //WE'RE NOT EVEN IN A ROOM TO CHECK
 
+            for (int i = 0; i < myRoom.abstractRoom.creatures.Count; i++)
+            {
+                if (myRoom.abstractRoom.creatures[i].realizedCreature is Player checkPlayer
+                    && checkPlayer.slugcatStats.name.value == "Guide"
+                    && checkPlayer.room != null //&& checkPlayer.room == myRoom //MAKE SURE THEY ARE IN OUR ROOM. or maybe not...
+                    && !(checkPlayer.dead || checkPlayer.inShortcut) //AND VALID 
+                )
+                {
+                    return checkPlayer; //GUIDE FOUND! RETURN THE REFERENCE
+                }
+            }
+            return null; //NO GUIDE FOUND. RETURN NULL
+        }
 
         private void ScavengerAI_DecideBehavior(On.ScavengerAI.orig_DecideBehavior orig, ScavengerAI self)
         {
             orig(self);
-            if ((self.scavenger.room.game.Players[0].realizedCreature as Player).slugcatStats.name.value == "Guide")
+            //if ((self.scavenger.room.game.Players[0].realizedCreature as Player).slugcatStats.name.value == "Guide")
+            Player closeGuide = FindNearbyGuide(self.scavenger.room);
+            if (closeGuide != null)
             {
                 
                 //if no threat detected, if has food item > set destination to player
                 if (self.behavior == ScavengerAI.Behavior.Idle)
                 {
-                    self.SetDestination(self.scavenger.room.game.Players[0].pos);
+                    self.SetDestination(closeGuide.abstractCreature.pos); //self.scavenger.room.game.Players[0].pos
                 }
             }
             
@@ -89,17 +123,17 @@ namespace GuideSlugBase
         private void Grasp_ctor(On.Creature.Grasp.orig_ctor orig, Creature.Grasp self, Creature grabber, PhysicalObject grabbed, int graspUsed, int chunkGrabbed, Creature.Grasp.Shareability shareability, float dominance, bool pacifying)
         {
             orig(self, grabber, grabbed, graspUsed, chunkGrabbed, shareability, dominance, pacifying);
-            if ((self.grabber.room.game.Players[0].realizedCreature as Player).slugcatStats.name.value == "Guide")
+            //WW- CHECK IF OUR GRABBED OBJECT IS GUIDE
+            if (grabbed is Player && (grabbed as Player).slugcatStats.name.value == "Guide")
             {
-                
-                if ((self.grabber.room.game.Players[0].realizedCreature as Player).grabbedBy.Count > 0 && slippery == true)
+                if (grabbed.grabbedBy.Count > 0 && slippery == true)
                 {
                     grabber.ReleaseGrasp(graspUsed);
                     grabbed.AllGraspsLetGoOfThisObject(true);
                     slipperyTime = 40 * 15;
                     slippery = true;
                 }
-                if ((self.grabber.room.game.Players[0].realizedCreature as Player).grabbedBy.Count > 0 && !slippery && tailAttached)
+                if (grabbed.grabbedBy.Count > 0 && !slippery && tailAttached)
                 {
                     //set tailAttached to false, set hasTail to false
                     //break tail connection
@@ -121,7 +155,7 @@ namespace GuideSlugBase
         private void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
         {
             orig(self, eu);
-            if (self.room?.game?.Players[0]?.realizedCreature is Player player && player?.slugcatStats.name.value == "Guide")
+            if (self.slugcatStats.name.value == "Guide")
             {
                 //when underwater, slippery = true, countdown starts
                 if (self.animation == Player.AnimationIndex.DeepSwim)
@@ -219,7 +253,7 @@ namespace GuideSlugBase
         public void SetupDMSSprites()
         {
             //-- The ID of the spritesheet we will be using as the default sprites for our slugcat
-            var sheetID = "aveskori.applecat";
+            var sheetID = "aveskori.guide";
 
             //-- Each player slot (0, 1, 2, 3) can be customized individually
             for (int i = 0; i < 4; i++)
@@ -227,7 +261,7 @@ namespace GuideSlugBase
                 SpriteDefinitions.AddSlugcatDefault(new Customization()
                 {
                     //-- Make sure to use the same ID as the one used for our slugcat
-                    Slugcat = "aveskori.applecat",
+                    Slugcat = "Guide",
                     PlayerNumber = i,
                     CustomSprites = new List<CustomSprite>
                     {
@@ -237,19 +271,22 @@ namespace GuideSlugBase
                         
                         new CustomSprite() { Sprite = "BODY", SpriteSheetID = sheetID, Color = Color.white },
                         new CustomSprite() { Sprite = "ARMS", SpriteSheetID = sheetID, Color = Color.white },
+                        new CustomSprite() { Sprite = "HIPS", SpriteSheetID = sheetID, Color = Color.white },
+                        new CustomSprite() { Sprite = "LEGS", SpriteSheetID = sheetID, Color = Color.white },
                         new CustomSprite() { Sprite = "HIPSRIGHT", SpriteSheetID = sheetID, Color = Color.white },
                         new CustomSprite() { Sprite = "HIPSLEFT", SpriteSheetID = sheetID, Color = Color.white },
                         new CustomSprite() { Sprite = "TAIL", SpriteSheetID = sheetID, Color = Color.white },
                         new CustomSprite() { Sprite = "TAILSPOTS", SpriteSheetID = sheetID, Color = Color.white }
                     },
 
-                    //-- Customizing the tail size and color is also supported, values should be set between 0 and 1
-                    CustomTail = new CustomTail()
-                    {
-                        Length = i / 4f,
-                        Wideness = i / 6f,
-                        Roundness = 0.9f
-                    }
+                    //-- Customizing the tail size and color is also supported, remove these lines if you want your slugcat to use the default shape
+                    //CustomTail = new CustomTail()
+                    //{
+                    //    Length = 4f,
+                    //    Wideness = 1f,
+                    //    Roundness = 0.1f,
+                    //    ForbidTailResize = false //(I updated these a while back. new defaults)
+                    //}
                 });
             }
         }
